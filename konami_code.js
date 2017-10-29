2bcc822228818d05182adb906877cc737a39e8e7
Alishaan Asaria <alishaan.asaria@gmail.com>
2017-10-29 03:08:46 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,14 +7,9 @@ function init() {
   const body = document.querySelector('body');
 
   body.addEventListener('keydown', function(e) {
-  // get the value of the key code from the key map
-  // var key = allowedKeys[e.keyCode];
   const key = parseInt(e.detail || e.which);
-  // compare the key with the required key
     if (key == code[konamiCodePosition]) {
-      // move to the next key in the konami code sequence
       konamiCodePosition++;
-      // if the last key is reached, activate cheats
       if (konamiCodePosition == code.length) {
           alert("Hurray!");
           konamiCodePosition = 0;
