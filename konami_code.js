a9d65f968b958c103126c51b59ecda8650b1d0b1
Alishaan Asaria <alishaan.asaria@gmail.com>
2017-10-29 03:09:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,20 +6,25 @@ function init() {n
   const body = document.querySelector('body');
 
   body.addEventListener('keydown', function(e) {
-    
+
     const key = parseInt(e.detail || e.which);
-    
+
     if (key == code[konamiCodePosition]) {
-      
+
       konamiCodePosition++;
-      
+
       if (konamiCodePosition == code.length) {
+        
           alert("Hurray!");
           konamiCodePosition = 0;
+          
       }
-      
+
     }else {
+      
         konamiCodePosition = 0;
+        
     }
+    
   });
 }
