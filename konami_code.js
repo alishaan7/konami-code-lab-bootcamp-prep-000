8734dd09703f294066324ad7ec1cbe49cab965da
Alishaan Asaria <alishaan.asaria@gmail.com>
2017-10-29 03:00:13 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -8,4 +8,26 @@ function init() {
       return e.preventDefault()
     }
   })
+  
+  document.addEventListener('keydown', function(e) {
+  // get the value of the key code from the key map
+  var key = allowedKeys[e.keyCode];
+  // get the value of the required key from the konami code
+  var requiredKey = konamiCode[konamiCodePosition];
+
+  // compare the key with the required key
+    if (key == requiredKey) {
+
+      // move to the next key in the konami code sequence
+      konamiCodePosition++;
+
+      // if the last key is reached, activate cheats
+      if (konamiCodePosition == konamiCode.length) {
+          activateCheats();
+          konamiCodePosition = 0;
+      }
+    }else {
+        konamiCodePosition = 0;
+    }
+  });
 }
